d08fce5cd6ad1302dbbd3ffa2b3cc64ceedb0deb
Hill <hill@kimo.com>
2016-07-16 13:13:22 +0000

    Change to manually defined encode

--- html_parser.php
+++ html_parser.php
@@ -6,14 +6,11 @@ ${_INIT_CONFIG}[_CLASS] = __NAMESPACE__.'\html_parser';
 
 class html_parser extends \PMVC\PlugIn
 {
-    private $opts;
-    public function init()
-    {
-        $this->opts = array('convert_to_encoding'=>'utf-8');
-    }
-
     public function css($content,$selector)
     {
-        return html5qp('<>'.$content, $selector, $this->opts);
+        if (isset($this['fromEncoding'])) {
+            $content = mb_convert_encoding($content, 'UTF-8', $this['fromEncoding']);
+        }
+        return html5qp('<>'.$content, $selector);
     }
 }
--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 PMVC\Load::plug();
-PMVC\setPlugInFolder('../');
+PMVC\addPlugInFolders(['../']);
 class HtmlParserTest extends PHPUnit_Framework_TestCase
 {
     function testCssParse()
