4be6c39f9a2b8938e4033570e13edb87bd103469
Hill <hill@kimo.com>
2017-02-14 13:23:04 +0000

    Add parse function

--- README.md
+++ README.md
@@ -13,9 +13,23 @@ Html Parser
 ```
 $plug = \PMVC\plug('html_parser');
 $object = $plug->css($html, $your_css_$selector);
-var_dump($object->toHtml());
+var_dump($object->html());
 ```
+## Useful tip
+
+### text or html
+* object->text()
+* object->html()
+### get tag attribute
+* object->attr('href')
+### Process a list (QueryPath)
+* https://web.archive.org/web/20120504094800/http://api.querypath.org/docs/class_query_path.html
+* Get one index
+   * $DOMElement = QueryPath->get(int index) : start from zero
+   * \PMVC\plug('html_parser')->parse($DOMElement, $selector);
+
 ### Parse Engine
+   * Old Api(better): https://web.archive.org/web/20130123082558/http://api.querypath.org/docs/
    * https://github.com/technosophos/querypath
 
 ## Install with Composer
--- html_parser.php
+++ html_parser.php
@@ -1,16 +1,31 @@
 <?php
+
 namespace PMVC\PlugIn\html_parser;
-use html5qp;
+
+use Masterminds\HTML5;
+use QueryPath\DOMQuery;
+use PMVC\PlugIn;
 
 ${_INIT_CONFIG}[_CLASS] = __NAMESPACE__.'\html_parser';
 
-class html_parser extends \PMVC\PlugIn
+class html_parser extends PlugIn
 {
-    public function css($content,$selector)
+    public function css($html, $selector, array $options=[])
     {
         if (isset($this['fromEncoding'])) {
-            $content = mb_convert_encoding($content, 'UTF-8', $this['fromEncoding']);
+            $html = mb_convert_encoding($html, 'UTF-8', $this['fromEncoding']);
         }
-        return html5qp('<>'.$content, $selector);
+        $html5 = new HTML5();
+        $html = $html5->loadHTML($html);
+        return $this->parse($html, $selector, $options);
+    }
+
+    /**
+     * @return QueryPath\DOMQuery
+     * https://github.com/technosophos/querypath/blob/master/src/QueryPath/DOMQuery.php
+     */
+    public function parse($dom, $selector=null, array $options=[])
+    {
+        return new DOMQuery($dom, $selector, $options);
     }
 }
