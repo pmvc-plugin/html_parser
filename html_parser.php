fb05854030a70aebdd974a35ff354c0da482b273
Hill <hill@kimo.com>
2017-02-15 17:14:36 +0000

    improve reuse html5 object

--- html_parser.php
+++ html_parser.php
@@ -10,12 +10,23 @@ ${_INIT_CONFIG}[_CLASS] = __NAMESPACE__.'\html_parser';
 
 class html_parser extends PlugIn
 {
+
+    private $_html5;
+
+    private function _getHtml5Object()
+    {   
+        if (empty($this->_html5)) {
+            $this->_html5 = new HTML5();
+        }
+        return $this->_html5;
+    }
+
     public function css($html, $selector, array $options=[])
     {
         if (isset($this['fromEncoding'])) {
             $html = mb_convert_encoding($html, 'UTF-8', $this['fromEncoding']);
         }
-        $html5 = new HTML5();
+        $html5 = $this->_getHtml5Object();
         $html = $html5->loadHTML($html);
         return $this->parse($html, $selector, $options);
     }
